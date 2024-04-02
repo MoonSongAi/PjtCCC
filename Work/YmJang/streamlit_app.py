8cb1f6986dfbf00a8f3833a8ff7f45dcdd72d3af
yummijang <154862914+yummijang@users.noreply.github.com>
2024-04-02 12:22:55 +0900

    "04/02 -3 by YM"

--- Work/YmJang/streamlit_app.py
+++ Work/YmJang/streamlit_app.py
@@ -66,6 +66,14 @@ def main():
     if 'loaded_image' not in st.session_state:
         st.session_state.loaded_image = None
     
+    # color_options 딕셔너리 정의
+    color_options = {
+        "Red": ([0, 100, 100], [10, 255, 255]),
+        "Green": ([50, 100, 100], [70, 255, 255]),
+        "Blue": ([110, 100, 100], [130, 255, 255]),
+        "Yellow": ([25, 100, 100], [35, 255, 255]),
+        "Black": ([0, 0, 0], [180, 255, 30])  # HSV 범위 예시 수정
+    }
 
     with st.sidebar:
         with st.expander("Select Image",expanded=True):            
@@ -80,6 +88,9 @@ def main():
                     step=1  # 단계
             )
             button_enabled = uploaded_Image is not None
+            # 색상 선택 드롭다운 메뉴
+            selected_color_name = st.selectbox("이미지 전처리 color_options", options=list(color_options.keys()))
+            lower_hsv, upper_hsv = color_options[selected_color_name]
             process_image = st.button("Analysis Design file....", disabled=not button_enabled)
 
 
@@ -156,31 +167,37 @@ def main():
                         st.image(saved_image, width=100)  # 썸네일 이미지 표시
 
 ######################## 추가함 ###################
-    st.sidebar.title("이미지 처리 설정")
-    
-    color_options = {
-        "Red": ([0, 100, 100], [10, 255, 255]),
-        "Green": ([50, 100, 100], [70, 255, 255]),
-        "Blue": ([110, 100, 100], [130, 255, 255]),
-        "Yellow": ([25, 100, 100], [35, 255, 255]),
-        "Black": ([0, 0, 0], [180, 255, 30])  # HSV 범위 예시 수정
-    }
     
     selected_color_name = st.sidebar.selectbox("색상 선택", list(color_options.keys()))
     lower_hsv, upper_hsv = color_options[selected_color_name]
     
     # "모든 이미지 처리" 버튼을 메인 페이지에 배치
-    if st.button('모든 이미지 처리'):
+    if st.button('이미지 전처리'):
+        # 처리된 이미지를 저장할 리스트 초기화
+        if 'processed_images' not in st.session_state:
+            st.session_state.processed_images = []
+        
+        # 이미지 처리 및 저장
         if 'images_list' in st.session_state:
             for image_name in st.session_state.images_list:
                 image_path = os.path.join('path_to_saved_images', image_name)
                 pil_image = Image.open(image_path).convert('RGB')
                 processed_image = process_image_with_hsv_range(pil_image, np.array(lower_hsv), np.array(upper_hsv))
                 
-                # 처리된 이미지를 Streamlit을 통해 표시
-                st.image(processed_image, caption=f'처리된 이미지: {image_name}')
+                # 처리된 이미지를 session_state에 추가
+                st.session_state.processed_images.append(processed_image)
+        
+        # 처리된 이미지를 썸네일 형태로 표시
+        if 'processed_images' in st.session_state and st.session_state.processed_images:
+            cols = st.columns(len(st.session_state.processed_images))
+            for idx, processed_image in enumerate(st.session_state.processed_images):
+                with cols[idx]:
+                    # 썸네일 이미지 표시 (PIL.Image 객체를 사용하는 경우)
+                    st.caption(st.session_state.images_list[idx])
+                    processed_image.thumbnail((200, 200))
+                    st.image(processed_image, width=100)
         else:
-            st.write("이미지 목록이 비어 있습니다.")
+            st.write("이미지가 없습니다.")
 
 ###################################################
 
